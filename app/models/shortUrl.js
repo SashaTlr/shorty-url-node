24b5b6cec95e38d6300cc6d2b48e03a3fc61bbe0
Sasha Tlr <sashatailor@gmail.com>
2016-05-19 19:54:47 -0400

    Add required:true validation to shortcode

--- app/models/shortUrl.js
+++ app/models/shortUrl.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 
 var shortUrlSchema = new mongoose.Schema({
   url: {type: String, required: [true, 'url is not present']},
-  shortcode: {type: String, index: {unique: true}, validate: [/^[0-9a-zA-Z_]{4,}$/,
+  shortcode: {type: String, required: true, index: {unique: true}, validate: [/^[0-9a-zA-Z_]{4,}$/,
     'The shortcode fails to meet the following regexp: ^[0-9a-zA-Z_]{4,}$.']},
   updated_at: {type: Date, default: Date.now},
 });
