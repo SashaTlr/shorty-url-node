7e7b0bc879e07d89dafa51e44715e95a7a1bc2e9
Sasha Tlr <sashatailor@gmail.com>
2016-05-19 22:38:03 -0400

    Add tests for valid url and blank url

--- test/model.js
+++ test/model.js
@@ -10,15 +10,37 @@ var shortUrl = require('../app/models/shortUrl.js')
 
 describe ('Shortened URL model tests', function(){
 
+  var dbcount;
   before (function(done){
     mongoose.connect(mongoURI);
+    mongoose.connection.db.dropDatabase();
     done();
   });
 
-  describe('', function(){
+  describe ('Add objects to URL model', function() {
+    it('valid url and shortcode should be added to database', function(done){
+      new_url = {"shortcode":"abcdef", "url":"www.google.com"}
+      var addShortCode = new shortUrl(new_url);
+      addShortCode.save(function (err, docs){
+        if (err){
+          throw err
+        }
+          shortUrl.count(new_url, function(err, count){
+          count.should.equal(1);
+          done();
+        });
+      });
+    });
 
-    xit('', function(done){
-
+    it('blank url should not be added to database', function(done){
+      new_url = {"shortcode":"abcdef", "url":""}
+      var addShortCode = new shortUrl(new_url);
+      addShortCode.save(function (err, docs){
+          shortUrl.count(new_url, function(err, count){
+          count.should.equal(0);
+          done();
+        });
+      });
     });
 
   });
@@ -28,4 +50,4 @@ describe ('Shortened URL model tests', function(){
     done();
   });
 
-})
+});
